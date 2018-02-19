fadc390e413b3e057c2b2c1ef1a6889b12411c77
Ankit Bhatnagar <ankit.bhatnagar1994@gmail.com>
2018-02-19 12:12:05 +0530

    Propertyfiledata_DI

---
+++ PropertyFileData_DI/src/test/Client.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.omg.CORBA.portable.ApplicationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import beans.CP;
+
+
+public class Client {
+public static void main(String[] args) {
+	ApplicationContext ap= new ClassPathXmlApplicationContext("resources/spring.xml");
+
+	
+	
+	CP cp=(CP)ap.getBean("cp");
+	cp.createConnection();
+}
+}
